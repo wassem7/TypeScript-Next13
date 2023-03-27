0280890873ee209986962342790a7f4b1cc9c1b2
Wassem Darkwa <waseemdarkwa@gmail.com>
2023-03-27 13:52:51 +0100

    Next 13 routes

---
+++ app/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className='text-xl bg-green-400 p-5 text-white flex justify-center'>
+        Header
+      </h1>
+    </div>
+  );
+};
+
+export default Header;
---
+++ app/todos/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+  return (
+    <div>
+      <h1 className='text-3xl text-gray-400 mt-3'>Todos</h1>
+    </div>
+  );
+};
+
+export default page;
